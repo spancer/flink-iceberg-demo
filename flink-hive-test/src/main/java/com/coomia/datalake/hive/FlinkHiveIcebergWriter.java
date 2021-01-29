c4f5eee4cf9c176183f53a86eb90f6b0524b2928
spancer <leipeng@analysys.com.cn>
2021-01-29 11:10:53 +0800

    aa

--- flink-hive-test/src/main/java/com/coomia/datalake/hive/FlinkHiveIcebergWriter.java
+++ flink-hive-test/src/main/java/com/coomia/datalake/hive/FlinkHiveIcebergWriter.java
@@ -1,19 +1,14 @@
 package com.coomia.datalake.hive;
 
 import java.util.List;
+import java.util.UUID;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import org.apache.flink.table.data.ArrayData;
-import org.apache.flink.table.data.DecimalData;
-import org.apache.flink.table.data.MapData;
-import org.apache.flink.table.data.RawValueData;
+import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
-import org.apache.flink.table.data.StringData;
-import org.apache.flink.table.data.TimestampData;
 import org.apache.flink.types.Row;
-import org.apache.flink.types.RowKind;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
@@ -21,7 +16,6 @@ import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.flink.TableLoader;
-import org.apache.iceberg.flink.TableLoader.HadoopTableLoader;
 import org.apache.iceberg.flink.sink.FlinkSink;
 import org.apache.iceberg.hive.HiveCatalog;
 import org.apache.iceberg.types.Types;
@@ -57,27 +51,33 @@ public class FlinkHiveIcebergWriter {
     // create an iceberg table.
     Table table = catalog.createTable(name, schema, spec);
 
-    List<Row> rows = Lists.newArrayList(Row.of(1, "hello"), Row.of(2, "world"), Row.of(3, "foo"));
     DataStream<RowData> dataStream = env.addSource(new SourceFunction<RowData>() {
+      /**
+       * 
+       */
+      private static final long serialVersionUID = -8786053745984140851L;
+      boolean flag = true;
 
       @Override
       public void run(SourceContext<RowData> ctx) throws Exception {
-        while (true) {
-          ctx.collect(new RowData);
+        while (flag) {
+          GenericRowData row = new GenericRowData(2);
+          row.setField(0, System.currentTimeMillis());
+          row.setField(1, UUID.randomUUID().toString());
+          ctx.collect(row);
         }
       }
 
       @Override
       public void cancel() {
-        // TODO Auto-generated method stub
-
+        flag = false;
       }
     });
 
     TableLoader tableLoader = TableLoader.fromHadoopTable(table.location());
-    
-    FlinkSink.forRowData(dataStream).table(table).tableLoader(tableLoader)
-        .writeParallelism(1).build();
+
+    FlinkSink.forRowData(dataStream).table(table).tableLoader(tableLoader).writeParallelism(1)
+        .build();
 
     // Execute the program.
     env.execute("Test Iceberg DataStream");
